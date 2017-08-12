545e67fa3653b4031ae7cc887dbf69a4442ea320
Quang Nguyen <quang.bme.hust.55@gmail.com>
2017-08-12 11:06:52 +0900

    Let's see how we consume the library.

--- sample/src/main/java/com/quangnguyen/sample/MainActivity.java
+++ sample/src/main/java/com/quangnguyen/sample/MainActivity.java
@@ -2,12 +2,46 @@ package com.quangnguyen.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.quangnguyen.manga.doraemon.Doraemon;
+import com.quangnguyen.manga.doraemon.Nobita;
 
 public class MainActivity extends AppCompatActivity {
+
+  private Doraemon doraemon;
+  private Nobita nobita;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    // Our cute characters
+    doraemon = new Doraemon();
+    nobita = new Nobita();
+
+    // initialize views
+    populateViews();
+  }
+
+  private void populateViews() {
+    ImageView doraemonImage = findViewById(R.id.image_doraemon);
+    ImageView nobitaImage = findViewById(R.id.image_nobita);
+
+    // Set images
+    doraemonImage.setBackgroundResource(doraemon.getImage());
+    nobitaImage.setBackgroundResource(nobita.getImage());
+  }
+
+  public void onClick(View v) {
+    String name = "";
+    if (v.getId() == R.id.image_doraemon) {
+      name = doraemon.getName();
+    } else if (v.getId() == R.id.image_nobita) {
+      name = nobita.getName();
+    }
+    Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
   }
 }
